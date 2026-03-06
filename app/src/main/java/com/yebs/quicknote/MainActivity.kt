279251ea088b92329c447d8d9963911d15bc936a
Yirius125 <101884827+YEBS125@users.noreply.github.com>
2026-03-05 22:48:38 -0600

    Layout Create Note

--- app/src/main/java/com/yebs/quicknote/MainActivity.kt
+++ app/src/main/java/com/yebs/quicknote/MainActivity.kt
@@ -15,17 +15,8 @@ import com.yebs.quicknote.ui.theme.QuickNoteTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            QuickNoteTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
+
+        setContentView(R.layout.login_main)
     }
 }
 
