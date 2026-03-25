5e04133233816fdc989406006f9061b0716aee32
DarkNeto369 <ersagon39@gmail.com>
2026-03-25 01:10:39 -0600

    Pantallas Segundo Avance

---
+++ app/src/main/java/com/yebs/quicknote/CrearTextoActivity.kt
@@ -0,0 +1,20 @@
+package com.yebs.quicknote
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class CrearTextoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.crear_texto_activity)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
--- app/src/main/java/com/yebs/quicknote/LoginActivity.kt
+++ app/src/main/java/com/yebs/quicknote/LoginActivity.kt
@@ -2,10 +2,6 @@ package com.yebs.quicknote
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -16,7 +12,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.login_main)
+        setContentView(R.layout.login_activity)
     }
 }
 
